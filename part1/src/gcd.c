e4b31e31f2e5c971bf16c4623c7822bb9ebf0fb5
derick <derickwang@mine.local>
2025-02-05 16:51:29 -0500

    Initial commit - create needed files for part 1

--- part1/src/gcd.c
+++ part1/src/gcd.c
@@ -4,5 +4,13 @@
 #include "recursive.h"
 
 int main(int argc, char **argv) {
+	if(argc != 3){
+		fprintf(stderr, "Usage: ./gcd <integer m> <integer n>\n");
+		return EXIt_FAILURE;
+	}
+	int n = atoi(argv[1]), m = atoi(argv[2]);
+	if(n == 0 && m == 0){
+		printf("gcd(0, 0) = undefined\n");
+	}
     return EXIT_SUCCESS;
 }
