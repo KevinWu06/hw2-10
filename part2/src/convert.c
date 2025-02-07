b0edb39c03b896c303ed00ce36d03d440bd7e6ff
derick <derickwang@mine.local>
2025-02-07 17:23:50 -0500

    finish makefile for part2

--- part2/src/convert.c
+++ part2/src/convert.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(int argc, char **argv) {
+    int x;
+    printf("Enter an integer : ");
+    scanf("%d", &x);
+    printf("\nsigned decimal   : %d\n", x);
     return EXIT_SUCCESS;
 }
